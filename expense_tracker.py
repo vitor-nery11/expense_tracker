82be1871c18ab0cdd6e00887d068277d82bc6687
vitor-nery11 <vitornerysantos24@gmail.com>
2026-06-26 08:27:42 -0300

    feat: add three new functions

---
+++ expense_tracker.py
@@ -0,0 +1,27 @@
+def main():
+    print('running expense tracker!')
+
+    # get user input for expense.
+    get_user_expense()
+    
+    # Write their expense to a file 
+
+    # Read file and summarize expenses.
+    pass
+
+def get_user_expense():
+    print('Running expense tracker')
+
+def save_expense_to_file():
+    print(f' saving user expense')
+
+def summarize_expenses():
+    print(f'Summarize user expense')
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+    
