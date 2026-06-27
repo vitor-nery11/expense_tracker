bd717ab294038503cf6116c23709adc96a553dbf
vitor-nery11 <vitornerysantos24@gmail.com>
2026-06-27 13:11:06 -0300

    feat: going up function get_user_expense and improving summarize_expenses

--- expense.py
+++ expense.py
@@ -2,4 +2,8 @@ class Expense:
     def __init__(self, name, category, amount):
         self.name = name
         self.category = category 
-        self.amout = amount 
+        self.amount = amount 
+
+
+    def __repr__(self):
+         return  f'<Expense>: {self.name}, {self.category}, $ {self.amount:.2f}'
--- expense_tracker.py
+++ expense_tracker.py
@@ -1,14 +1,18 @@
+from expense import Expense 
+
 def main():
     print('running expense tracker!')
+    expense_file_path = 'expenses.csv'
 
     # get user input for expense.
-    get_user_expense()
+    expense = get_user_expense()
+    print(expense)
     
     # Write their expense to a file 
-    save_expense_to_file()
+    save_expense_to_file(expense, expense_file_path)
 
     # Read file and summarize expenses.
-    summarize_expenses()
+    summarize_expenses(expense_file_path)
 
 
 
@@ -23,24 +27,41 @@ def get_user_expense():
 
 
     while True:
-        print('Select a category: ')
+        print("Select a category: ")
         for i, category_name in enumerate(expense_categories):
-            print(f'{i + 1}. {category_name}')
+            print(f"  {i + 1}. {category_name}")
 
-        value_range = f' 1 - {len(expense_categories)}'
-        select_index = int(input(f'Enter a category number {value_range}:s'))
-        break
+        value_range = f"[1 - {len(expense_categories)}]"
+        selected_index = int(input(f"Enter a category number {value_range}: ")) - 1
+
+        if selected_index in range(len(expense_categories)):
+            selected_category = expense_categories[selected_index]
+            new_expense = Expense(
+                name=expense_name, category=selected_category, amount=expense_amount
+            )
+            return new_expense
+        else:
+            print("Invalid category. Please try again!")
+        
+
+def save_expense_to_file(expense: Expense, expense_file_path):
+    print(f'Saving user expense: {expense} to {expense_file_path}')
+    with open(expense_file_path, 'a') as f:
+        f.write(f'{expense.name},{expense.amount},{expense.category}\n')
 
 
-
-def save_expense_to_file():
-    print(f'Saving user expense')
-
-
-def summarize_expenses():
+def summarize_expenses(expense_file_path):
     print(f'Summarize user expense')
-
-
+    with open(expense_file_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            stripped_line = line.strip() 
+            expense_name, expense_amount, expense_category = line.strip().split(',')
+            print(expense_name, expense_amount, expense_category)
+            line_expense = Expense(name=expense_name, amount=float(expense_amount), category=expense_category)
+            print(line_expense)
+            expenses.append(line_expense)
+    print(expenses)
 
 
 
